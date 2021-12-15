92d63ae32d3ae9b5fa2d00296814c6a323df86a8
Technote <technote.space@gmail.com>
2021-12-15 17:39:18 +0900

    fix: type errors

--- src/strict-dependencies/resolveImportPath.ts
+++ src/strict-dependencies/resolveImportPath.ts
@@ -1,6 +1,14 @@
 import fs from 'fs';
 import path from 'path';
 
+type Tsconfig = {
+  extends?: string;
+  compilerOptions?: {
+    baseUrl?: string;
+    paths?: { [key: string]: Array<string> };
+  };
+}
+
 /**
  * import文のrootからのパスを求める
  */
@@ -13,11 +21,13 @@ export default (importPath: string, relativeFilePath?: string): string => {
   try {
     const tsConfigFile = fs.readFileSync(path.join(process.cwd(), '/tsconfig.json'), 'utf-8');
     // Exists ts config
-    const tsConfig = JSON.parse(tsConfigFile);
-    if (tsConfig.compilerOptions && tsConfig.compilerOptions.paths) {
-      Object.keys(tsConfig.compilerOptions.paths).forEach((key) => {
+    const tsConfig = JSON.parse(tsConfigFile) as Tsconfig;
+    const compilerOptions = tsConfig.compilerOptions;
+    if (compilerOptions && compilerOptions.paths) {
+      const paths = compilerOptions.paths;
+      Object.keys(paths).forEach((key) => {
         // FIXME: このlint ruleではimport先が存在するかチェックしておらず、複数のパスから正しい方を選択できないため[0]固定
-        importAliasMap[key] = tsConfig.compilerOptions.baseUrl ? path.join(tsConfig.compilerOptions.baseUrl, tsConfig.compilerOptions.paths[key][0]) : tsConfig.compilerOptions.paths[key][0];
+        importAliasMap[key] = compilerOptions.baseUrl ? path.join(compilerOptions.baseUrl, paths[key][0]) : paths[key][0];
       });
     }
   } catch (error) {
